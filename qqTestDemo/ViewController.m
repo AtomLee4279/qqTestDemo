cb8b17df7add0d29923cf07d0b5915fc4e4f3c5b
李一贤 <liyixian@atomlee-imac.local>
2018-03-08 12:38:42 +0800

    实现点击键盘“return”键及键盘以外的区域，退出键盘

--- qqTestDemo/ViewController.m
+++ qqTestDemo/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)login;
+@property (weak, nonatomic) IBOutlet UITextField *qqName;
+@property (weak, nonatomic) IBOutlet UITextField *qqPassWord;
 
 @end
 
@@ -16,6 +19,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.qqName.delegate = self;
+    self.qqPassWord.delegate = self;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +31,22 @@
 }
 
 
+- (IBAction)login
+{
+    NSLog(@"%@,%@",self.qqName.text,self.qqPassWord.text);
+}
+
+//实现UITextFieldDelegate代理中的textFieldShouldReturn方法，点击文本输入框ruturn键后退出键盘
+- (BOOL)textFieldShouldReturn:(UITextField *)textField// called when 'return' key pressed. return NO to ignore.
+{
+    [textField resignFirstResponder];//resign，释放第一响应者；UITextField的上上层基类是UIResponder
+    return  YES;
+}
+
+//// 重写基类UIResponder中的触摸完成方法，实现点击键盘以外的区域退出键盘
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];//调用UIView中接口关掉键盘
+    
+}
 @end
